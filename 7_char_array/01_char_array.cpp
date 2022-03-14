a3231d38875143180f64e78d288bcdb402ec2b10
Rajkumar Soni <rajksoni029@gmail.com>
2022-03-14 10:22:31 +0530

    day 14 code added

---
+++ 7_char_array/01_char_array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// char arr[4];
+	// arr = {'r', 'a', 'j'};
+
+	// char arr[4];
+	// arr = "raj";
+
+	// int arr[3]; //= "123";
+	// arr = {1, 3, 4};
+
+	// char arr[10] = "raj\0kumar";
+
+	// cout << arr << endl;
+
+	char arr[11] = {49, 82, 99, 42, 98, 49, 97, 0, 100, 101};
+
+	cout << arr << endl;
+	// for (int i = 0; i < 8; i++)
+	// 	cout << arr[i] << endl;
+	return 0;
+}
+
+/*
+'\0'
+NULL
+nullptr
+0
+0.0
+0x00
+0o0
+0b0
+*/
---
+++ 7_char_array/02_char_array_in_functions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int length(char arr[]){
+	int index = 0;
+	char ele = arr[index];
+
+	while(ele != '\0'){
+		ele = arr[++index];
+	}
+	return index;
+}
+
+bool arrCpy(const char src[], char dst[]){
+
+
+	return true;
+	// return false;
+}
+
+int main(){
+	// char arr[4];
+	// arr = {'r', 'a', 'j'};
+	// char arr[11] = {49, 82, 99, 42, 98, 49, 97, 0, 100, 101};
+
+	char arr1[10] = "raj\0kumar";
+	char arr2[10];
+	cout << length(arr1) << endl;
+	cout << arrCpy(arr1, arr2) << endl; // true
+
+	cout << arr1 << endl; // raj
+	cout << arr2 << endl; // raj
+
+	return 0;
+}
