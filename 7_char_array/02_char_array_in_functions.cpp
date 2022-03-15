8a9c10844102bee19ddf08780e818c244bc80512
Rajkumar Soni <rajksoni029@gmail.com>
2022-03-15 10:14:36 +0530

    code updated

--- 7_char_array/02_char_array_in_functions.cpp
+++ 7_char_array/02_char_array_in_functions.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int length(char arr[]){
+int length(const char arr[]){
 	int index = 0;
 	char ele = arr[index];
 
@@ -11,10 +11,19 @@ int length(char arr[]){
 	return index;
 }
 
-bool arrCpy(const char src[], char dst[]){
-
-	return true;
-	// return false;
+bool arrCpy(const char src[], char dst[], int dst_size){
+	int src_size = length(src);
+	if (src_size < dst_size){ 
+		int i = 0;
+		char temp = src[i];
+		while(temp != '\0'){
+			dst[i] = src[i];
+			temp = src[++i];
+		}
+		dst[i] = '\0';
+		return true;
+	}
+	return false;
 }
 
 int main(){
@@ -22,13 +31,14 @@ int main(){
 	// arr = {'r', 'a', 'j'};
 	// char arr[11] = {49, 82, 99, 42, 98, 49, 97, 0, 100, 101};
 
-	char arr1[10] = "raj\0kumar";
-	char arr2[10];
+	char arr1[10] = "raj kumar";
+	char arr2[length(arr1)+1];
 	cout << length(arr1) << endl;
-	cout << arrCpy(arr1, arr2) << endl; // true
+	cout << arrCpy(arr1, arr2, length(arr1)+1) << endl; // true
 
 	cout << arr1 << endl; // raj
 	cout << arr2 << endl; // raj
 
+	cout << length(arr2) << endl;
 	return 0;
 }
---
+++ 7_char_array/03_strconcat.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool strcat(){
+
+}
+
+bool strcomp(){
+
+}
+
+int main(){
+	char str1[5] = "Raj ";
+	char str2[5] = "Soni";
+	char arr[1] = "";
+
+	cout << arr << endl;
+
+	cout << &str1 << endl;
+	cout << &str2 << endl;
+	cout << &arr << endl;
+
+
+	char name[10];
+	// strcat(name, 10, str1, str2);
+
+	return 0;
+}
