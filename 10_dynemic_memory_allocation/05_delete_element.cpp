34a63681dddbdefdb518624cde6d98ba76627706
Rajkumar Soni <rajksoni029@gmail.com>
2022-04-13 09:51:31 +0530

    day 24

--- 10_dynemic_memory_allocation/05_delete_element.cpp
+++ 10_dynemic_memory_allocation/05_delete_element.cpp
@@ -33,7 +33,30 @@ void add_dynemic(int ele){
 
 void remove_element(){
     // Start Code Here
+    if (SIZE==0){
+        cout << "Array is Empty" << endl;
+        return;
+    }
 
+    if (count == SIZE){
+        arr[count-1] = 0;
+        count--;
+    }
+    else {
+        SIZE = SIZE-2;
+        // create new array of size - 2
+        int *tempArr = new int[SIZE]{};
+
+        // copy elements from old arr to tempArr
+        for (int i = 0; i < SIZE; i++){
+            tempArr[i] = arr[i];
+        }
+
+        // delete old array and point to new array
+        delete[] arr;
+        arr = tempArr;
+        count--;
+    }
 
     // End Code Here
     for (int i = 0; i < SIZE; i++){
@@ -50,6 +73,8 @@ int main(){
     add_dynemic(4);   // 2, 5, 8, 1, 4, 0
     add_dynemic(3);   // 2, 5, 8, 1, 4, 3
     add_dynemic(9);   // 2, 5, 8, 1, 4, 3, 9, 0
+    add_dynemic(6);   // 2, 5, 8, 1, 4, 3, 9, 6
+    remove_element(); // 2, 5, 8, 1, 4, 3, 9, 0
     remove_element(); // 2, 5, 8, 1, 4, 3
     remove_element(); // 2, 5, 8, 1, 4, 0
     remove_element(); // 2, 5, 8, 1
